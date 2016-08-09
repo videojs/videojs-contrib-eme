b5a1207ef5dc5b46c121a2d636dbde01a155110c
Garrett Singer <gesinger@gmail.com>
2016-08-09 16:17:14 -0400

    Initial commit

---
+++ src/plugin.js
@@ -0,0 +1,42 @@
+import videojs from 'video.js';
+
+// Default options for the plugin.
+const defaults = {};
+
+/**
+ * Function to invoke when the player is ready.
+ *
+ * This is a great place for your plugin to initialize itself. When this
+ * function is called, the player will have its DOM and child components
+ * in place.
+ *
+ * @function onPlayerReady
+ * @param    {Player} player
+ * @param    {Object} [options={}]
+ */
+const onPlayerReady = (player, options) => {
+  player.addClass('vjs-contrib-eme');
+};
+
+/**
+ * A video.js plugin.
+ *
+ * In the plugin function, the value of `this` is a video.js `Player`
+ * instance. You cannot rely on the player being in a "ready" state here,
+ * depending on how the plugin is invoked. This may or may not be important
+ * to you; if not, remove the wait for "ready"!
+ *
+ * @function contribEme
+ * @param    {Object} [options={}]
+ *           An object of options left to the plugin author to define.
+ */
+const contribEme = function(options) {
+  this.ready(() => {
+    onPlayerReady(this, videojs.mergeOptions(defaults, options));
+  });
+};
+
+// Register the plugin with video.js.
+videojs.plugin('contribEme', contribEme);
+
+export default contribEme;
