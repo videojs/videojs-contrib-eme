a86177aecdb00ccc1df14eaee311aab0ba81a27e
Chuck Wilson <chuck.wilson+github@gmail.com>
2018-11-12 12:09:38 -0500

    this should be null

--- src/plugin.js
+++ src/plugin.js
@@ -303,7 +303,7 @@ const eme = function(options = {}) {
       // fake an encrypted event for handleEncryptedEvent
       const mockEncryptedEvent = {
         initDataType: 'cenc',
-        initData: new Uint8Array(),
+        initData: null,
         target: player.tech_.el_
       };
 
