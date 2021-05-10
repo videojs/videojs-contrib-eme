9d14d38c292a4dcc77ca683d95f3e57f9f2d519a
brandonocasey <brandonocasey@gmail.com>
2021-05-10 11:43:00 -0400

    only run on safari/chrome/firefox

--- scripts/karma.conf.js
+++ scripts/karma.conf.js
@@ -4,7 +4,14 @@ module.exports = function(config) {
 
   // see https://github.com/videojs/videojs-generate-karma-config
   // for options
-  const options = {};
+  const options = {
+    browserstackLaunchers(defaults) {
+      delete defaults.bsSafariElCapitan;
+      delete defaults.bsEdgeWin10;
+      delete defaults.bsIE11Win10;
+      return defaults;
+    }
+  };
 
   config = generate(config, options);
 
