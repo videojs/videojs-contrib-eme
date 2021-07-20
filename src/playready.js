915efb74cce9bd0ceb0d6a2be5c581ae58e43112
Gary Katsevman <git@gkatsev.com>
2021-07-20 14:02:38 -0400

    report response body as cause for playready

--- src/playready.js
+++ src/playready.js
@@ -64,8 +64,9 @@ export const requestPlayreadyLicense = (keySystemOptions, messageBuffer, emeOpti
     }
 
     if (response.statusCode >= 400 && response.statusCode <= 599) {
-      // Pass an empty object as the error to use the default code 5 error message
-      callback({});
+      const cause = String.fromCharCode.apply(null, new Uint8Array(responseBody));
+
+      callback({cause});
       return;
     }
 
