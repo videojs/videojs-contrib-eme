a90edcbb50a4671897f33626dd2b4c32859106f6
Adam Waldron <awaldron@brightcove.com>
2024-02-15 15:13:28 -0800

    fix: disable firefox headless in karma (#205)

--- scripts/karma.conf.js
+++ scripts/karma.conf.js
@@ -4,7 +4,16 @@ module.exports = function(config) {
 
   // see https://github.com/videojs/videojs-generate-karma-config
   // for options
-  const options = {};
+  const options = {
+    browsers(aboutToRun) {
+      // TODO - current firefox headless fails to run w karma, blocking the npm version script.
+      // We should look into a better workaround that allows us to still run firefox through karma
+      // See https://github.com/karma-runner/karma-firefox-launcher/issues/328
+      return aboutToRun.filter(function(launcherName) {
+        return launcherName !== 'FirefoxHeadless';
+      });
+    }
+  };
 
   config = generate(config, options);
 
